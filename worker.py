e3ee258002f85060cbbe2005ddd7058011f34c62
mynam333 <39339180+mynam333@users.noreply.github.com>
2018-05-17 05:39:07 +0900

    Update worker.py

--- worker.py
+++ worker.py
@@ -5,11 +5,11 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('bot_token', 'NDQ2MjcwNTk4NTE2MzEwMDE3.Dd4oLw.42gJ0sj3OUQ2ywG8RGMo_TufDpA')
 
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
     with Connection(conn):
-        worker = Worker(map(Queue, listen))
+        worker = Worker(map(Queue))
         worker.work()
