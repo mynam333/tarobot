5e9bc091bc56ae95c9f1ddb9ecf47afe1c03d091
mynam333 <39339180+mynam333@users.noreply.github.com>
2018-05-17 05:42:41 +0900

    Update worker.py

--- worker.py
+++ worker.py
@@ -3,8 +3,6 @@ import os
 import redis
 from rq import Worker, Queue, Connection
 
-listen = ['high', 'default', 'low']
-
 redis_url = os.getenv('bot_token', 'NDQ2MjcwNTk4NTE2MzEwMDE3.Dd4oLw.42gJ0sj3OUQ2ywG8RGMo_TufDpA')
 
 conn = redis.from_url(redis_url)
