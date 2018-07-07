044778fa424394ca944b1a84de49ba0a01990cb2
mynam333 <39339180+mynam333@users.noreply.github.com>
2018-07-08 08:43:51 +0900

    Update TaroBot.py

--- TaroBot.py
+++ TaroBot.py
@@ -713,8 +713,8 @@ async def on_message(message):
          
     elif message.content.startswith(';목록'):
          embed=discord.Embed(title="★***거미콘 명령어 목록입니다.***", color=0xffff00)
-         embed.add_field(name="```;야호```", value="```https://i.imgur.com/xXk8nVo.gif```")
-         embed.add_field(name="```;삐짐```", value="```https://i.imgur.com/64vOuO4.png```")
+         embed.add_field(name="```;야호```", value="https://i.imgur.com/xXk8nVo.gif")
+         embed.add_field(name="```;삐짐```", value="https://i.imgur.com/64vOuO4.png")
          embed.set_footer(text="Made by 유아루")
          await client.send_message(message.channel, embed=embed)
          
